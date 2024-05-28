77cedb19dc200f9a93e1eff4404fc63f15b3ab8b
oneknowchar <oneknowchar@gmail.com>
2024-05-29 01:01:37 +0900

    react component name MUST start with UPPERCASE

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  let title = '강남 우동 맛집zz';
-  
+  let [title, titleFunc] = useState(['남자코트 추천', '강남 우동 맛집', '고양이와 강아지']);
+  let [likeNum, likeNumFunc] = useState(0);
+
   return (
     <div className="App">
       <div>
-        <h4 className="black-nav" style={{color:'red', fontSize:'50px'}}>it's blog</h4>
+        <h4 className="black-nav">ReactBlog</h4>
       </div>
-      <h4>{ title }</h4>
+      <button onClick={()=>{
+        let copy = [...title];
+        copy.sort();
+        titleFunc(copy);
+      }}>가나다순 정렬</button>
+      <button onClick={()=>{
+        let copy = [...title];
+        copy[0] = '여자코트 추천';
+        titleFunc(copy);
+      }}>글 수정</button>
+      <div className='list'>
+        <h4>{title[0]} <span onClick={()=>{likeNumFunc(likeNum+1)}}>👍</span> {likeNum} </h4>
+        <p>2월 17일 발행</p> 
+      </div>
+      <div className='list'>
+        <h4>{title[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{title[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <Modal></Modal>
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
