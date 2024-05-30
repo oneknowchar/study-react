d3bdbb098971f94ef325ba4b816da6c4bf91e918
oneknowchar <oneknowchar@gmail.com>
2024-05-30 13:36:22 +0900

    Update boostrap banner, container(row, col)

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,8 +3,6 @@ import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-
   return (
     <div className="App">
       <Navbar bg="primary" data-bs-theme="dark">
@@ -12,11 +10,31 @@ function App() {
           <Navbar.Brand href="#home">Shopping</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className='main-bg'></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img alt='img1' src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img alt='img2' src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img alt='img3' src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
