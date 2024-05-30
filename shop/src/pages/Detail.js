db0b04291c4e42eb2e3b89e6a067f6e378785e27
oneknowchar <oneknowchar@gmail.com>
2024-05-30 18:15:06 +0900

    useEffect ( mount, update)

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -1,11 +1,23 @@
+import userEvent from "@testing-library/user-event";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+let num = 0;
 function Detail(props) {
+
+  //It works after rendering HTML, Use it when some works take long time
+  //Goal : Show HTML first, Rapid service experience for user
+  useEffect(function(){
+    num++;
+    console.log('hello', num)
+  })
   let {id} = useParams();
+  let [count, countFunc] = useState(0);
   let item = props.shoes.find((x)=> x.id === Number(id));
   
   return (
     <div className="container">
+      <button onClick={()=>{countFunc(++count)}}>click</button> {count}
       <div className="row">
         <div className="col-md-6">
           <img alt="1" src={item.image} width="100%" />
