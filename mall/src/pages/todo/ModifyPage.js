bf770c15fb52bbd988bf3199dd4e13a178501cd3
oneknowchar <oneknowchar@gmail.com>
2024-06-08 23:11:24 +0900

    Update Modify page

---
+++ mall/src/pages/todo/ModifyPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ModifyPage = () => {
+  return <div className="text-3xl font-extrabold">ModifyPage</div>;
+};
+
+export default ModifyPage;
--- mall/src/pages/todo/ReadPage.js
+++ mall/src/pages/todo/ReadPage.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import {
+  createSearchParams,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from 'react-router-dom';
 
 const ReadPage = () => {
-  const { tno } = useParams();
-  console.log(tno);
+  const navigate = useNavigate();
 
-  return <div className={'text-3xl'}>ReadPage</div>;
+  const { tno } = useParams();
+  const [queryParams] = useSearchParams();
+
+  const page = queryParams.get('page') ? parseInt(queryParams.get('page')) : 1;
+  const size = queryParams.get('size') ? parseInt(queryParams.get('size')) : 10;
+
+  const queryStr = createSearchParams({ page, size }).toString();
+
+  const moveToModify = () => {
+    navigate({
+      pathname: `/todo/modify/${tno}`,
+      search: queryStr,
+    });
+  };
+  const moveToList = () => {
+    navigate({
+      pathname: `/todo/list`,
+      search: queryStr,
+    });
+  };
+
+  return (
+    <div className={'text-3xl'}>
+      Todo Read ReadPage {tno}
+      <div>
+        <button onClick={moveToModify}>Test Modify</button>
+        <button onClick={moveToList}>Test List</button>
+      </div>
+    </div>
+  );
 };
 
 export default ReadPage;
