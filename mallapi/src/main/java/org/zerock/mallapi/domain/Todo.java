50617ee6b88365d83dfea65c0da3d2de39c1d5d5
oneknowchar <oneknowchar@gmail.com>
2024-06-09 13:50:22 +0900

    JPA Entity, JpaRepository

---
+++ mallapi/src/main/java/org/zerock/mallapi/domain/Todo.java
@@ -0,0 +1,36 @@
+package org.zerock.mallapi.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "tbl_todo")
+public class Todo {
+	//Auto increment, sequence는 IDENTITY !!!
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long tno;
+	
+	@Column(length = 500, nullable = false)
+	private String title;
+	private String content;
+	private boolean complete;
+	private LocalDate dueDate;
+}
