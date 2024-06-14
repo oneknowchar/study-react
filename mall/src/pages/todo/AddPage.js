cf1ade6a1809b7c28569e9baacab5544e792a1c7
oneknowchar <oneknowchar@gmail.com>
2024-06-14 22:55:42 +0900

    todo Write page update

--- mall/src/pages/todo/AddPage.js
+++ mall/src/pages/todo/AddPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import AddComponent from '../../components/todo/AddComponent';
 
 const AddPage = () => {
-  return <div className={'text-3xl font-extrabold'}>AddPage</div>;
+  return (
+    <div className="p-4 w-full bg-white">
+      <div className="text-3xl font-extrabold">Add Page</div>
+
+      <AddComponent />
+    </div>
+  );
 };
 
 export default AddPage;
